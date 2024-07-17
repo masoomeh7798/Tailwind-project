4ea4a6cde62a7d72d3faaf20a391b7777e69088e
masoomeh7798 <masoomeh77ghorbani98@gmail.com>
2024-07-17 21:33:17 +0330

    navbar & dark light btn
    navbar simple form and dark/light created

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 "use strict"
+const darkLightBtn=document.querySelector('#dark-light')
+
+let isDark=false;
+darkLightBtn.addEventListener('click',()=>{
+    if(isDark){
+    document.documentElement.classList.remove('dark')
+    isDark=false;
+    }else{
+    document.documentElement.classList.add('dark')
+    isDark=true; 
+    }
+
+})
