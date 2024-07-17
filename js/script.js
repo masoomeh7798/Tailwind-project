f5e155032d55d02f8517301ee632414624ea9e5c
masoomeh7798 <masoomeh77ghorbani98@gmail.com>
2024-07-17 22:44:52 +0330

    navbar dark mode completed
    completed

--- js/script.js
+++ js/script.js
@@ -1,14 +1,18 @@
 "use strict"
+// start dark light button
 const darkLightBtn=document.querySelector('#dark-light')
+const logoEl=document.querySelector('#logo img')
 
 let isDark=false;
 darkLightBtn.addEventListener('click',()=>{
     if(isDark){
     document.documentElement.classList.remove('dark')
+    logoEl.src='assets/images/Porsche_wordmark.png'
     isDark=false;
     }else{
     document.documentElement.classList.add('dark')
     isDark=true; 
+    logoEl.src='assets/images/porsche-2-xxl2.png'
     }
-
 })
+// end dark light button
