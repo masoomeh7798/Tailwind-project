7a7c452bdc5471abc251ba7f38b15e1ccdfe2ec9
masoomeh7798 <masoomeh77ghorbani98@gmail.com>
2024-07-17 16:08:22 +0330

    base files created

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'selector',
+  content: ["./index.html","./js/script.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
